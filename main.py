cd85af03bc8c6041162b1470ce5ad4e99a848922
Praveena96-Reddy <57896190+Praveena96-Reddy@users.noreply.github.com>
2020-09-22 16:40:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# DELICATE - TAYLOR SWIFT#
+'''
+This Song is about meeting someone
+who you truly love and want to be with but then you suddenly start worrying
+about the things you think that they've heard about you prior to meeting you
+'''	
+
+ 
+SongName = "Delicate"
+Artist = "Taylor Swift" #she is the Lead Vocal , backing vocal amd on of the song writers
+WriterOne = "Max Martin"
+WriterTwo = "Karl Johan"
+Producer = "Max Martin"
+Language = "English"
+DurationInSeconds = 232
+YearOfPublished = 2019
+Genre = "ElectroPop"
+IsBestSongOfTheArtist = True #Delicate is one of the best songs of Taylor Swift
+
+'''
+She was nominated for various awards but
+Won - Titanium Awards (BMI Awards), Award Winning Song (BMI Awards) , Publisher of the Year (BMI Awards),
+Award Winning Song (ASCAP Awards), Best Music Video (iHeartRadio Music Awards)
+'''
+IsAnAwardWinningSong = True
